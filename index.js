46b8d00976b7b3bab5287b268e9a1a70bd56777d
ueessi <68377528+ueessi@users.noreply.github.com>
2020-08-08 11:46:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -50,9 +50,9 @@ async function msgHandler (client, message) {
         const { pushname } = sender
         const { formattedTitle } = chat
         const time = moment(t * 1000).format('DD/MM HH:mm:ss')
-        const commands = ['quotes', 'assalamualaikum', 'P', 'thul', 'tul', 'hai', 'hallo', 'hi', 'halo', 'bot', 'selamat pagi', 'pagi', 'selamat siang', 'siang', 'selamat malam', 'malam', 'makasi', 'makasih', 'thanks', 'maacii', 'mas', 'mbak', 'mba', 'dek']
+        const commands = ['#menu', '#help', 'quotes', 'assalamualaikum', 'P', 'thul', 'tul', 'hai', 'hallo', 'hi', 'halo', 'bot', 'selamat pagi', 'pagi', 'selamat siang', 'siang', 'selamat malam', 'malam', 'makasi', 'makasih', 'thanks', 'maacii', 'mas', 'mbak', 'mba', 'dek']
         const cmds = commands.map(x => x + '\\b').join('|'),
-        const cmd = type === 'chat' ? body.match(new RegExp(cmds, 'gi')) : ''
+        const cmd = type === 'chat' ? body.match(new RegExp(cmds, 'gi')) : type === 'image' && caption ? caption.match(new RegExp(cmds, 'gi')) : ''
 
         if (cmd) {
             if (!isGroupMsg) console.log(color('[EXEC]'), color(time, 'yellow'), color(cmd[0]), 'from', color(pushname))
@@ -60,6 +60,123 @@ async function msgHandler (client, message) {
             const args = body.trim().split(' ')
             const isUrl = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi);
             switch (cmd[0]) {
+                case '#menu':
+                case '#help':
+                    client.sendText(from, 'ngapain?')
+                    break
+                case '#sticker':
+                case '#stiker':
+                    if (isMedia) {
+                        const mediaData = await decryptMedia(message)
+                        const imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`
+                        await client.sendImageAsSticker(from, imageBase64)
+                    } else if (quotedMsg && quotedMsg.type == 'image') {
+                        const mediaData = await decryptMedia(quotedMsg)
+                        const imageBase64 = `data:${quotedMsg.mimetype};base64,${mediaData.toString('base64')}`
+                        await client.sendImageAsSticker(from, imageBase64)
+                    } else if (args.length == 2) {
+                        const url = args[1]
+                        if (url.match(isUrl)) {
+                            await client.sendStickerfromUrl(from, url, {method: 'get'})
+                                .then(r => { if (!r) client.sendText(from, 'Maaf, link yang kamu kirim tidak memuat gambar.') })
+                                .catch(err => console.log('Caught exception: ', err))
+                        } else {
+                            client.sendText(from, 'Maaf, link yang kamu kirim tidak valid.')
+                        }
+                    } else {
+                        client.sendText(from, 'gambarnya mana?')
+                    }
+                    break
+                case '#tiktok':
+                    if (args.length == 2) {
+                        const url = args[1]
+                        if (!url.match(isUrl) && !url.includes('tiktok.com')) return client.sendText(from, 'Maaf, link yang kamu kirim tidak valid')
+                        await tiktok(url)
+                            .then((videoMeta) => {
+                                const filename = videoMeta.authorMeta.name + '.mp4'
+                                const caps = `*Metadata:*\nUsername: ${videoMeta.authorMeta.name} \nMusic: ${videoMeta.musicMeta.musicName} \nView: ${videoMeta.playCount.toLocaleString()} \nLike: ${videoMeta.diggCount.toLocaleString()} \nComment: ${videoMeta.commentCount.toLocaleString()} \nShare: ${videoMeta.shareCount.toLocaleString()} \nCaption: ${videoMeta.text.trim() ? videoMeta.text : '-'} \n\nMaaf \nTerimakasih.`
+                                client.sendFileFromUrl(from,videoMeta.url, filename, videoMeta.NoWaterMark ? caps : `⚠ Video tanpa watermark tidak tersedia. \n\n${caps}`)
+                                    .catch(err => console.log('Caught exception: ', err))
+                            }).catch((err) => {
+                                client.sendText(from, 'Gagal mengambil metadata, link yang kamu kirim tidak valid')
+                            });
+                    }
+                    break
+                case '#ig':
+                case '#instagram':
+                    if (args.length == 2) {
+                        const url = args[1]
+                        if (!url.match(isUrl) && !url.includes('instagram.com')) return client.sendText(from, 'Maaf, link yang kamu kirim tidak valid')
+                        instagram(url)
+                            .then(async (videoMeta) => {
+                                const content = []
+                                for (var i = 0; i < videoMeta.length; i++) {
+                                    await urlShortener(videoMeta[i].video)
+                                        .then((result) => {
+                                            console.log('Shortlink: ' + result)
+                                            content.push(`${i+1}. ${result}`)
+                                        }).catch((err) => {
+                                            client.sendText(from, `Error, ` + err)
+                                        });
+                                }
+                                client.sendText(from, `Link Download:\n${content.join('\n')} \n\nMaaf \nTerimakasih.`)
+                            }).catch((err) => {
+                                console.error(err)
+                                if (err == 'Not a video') return client.sendText(from, `Error, tidak ada video di link yang kamu kirim`)
+                                client.sendText(from, `Error, user private atau link salah`)
+                            });
+                    }
+                    break
+                case '#twt':
+                case '#twitter':
+                    if (args.length == 2) {
+                        const url = args[1]
+                        if (!url.match(isUrl) && !url.includes('twitter.com') || url.includes('t.co')) return client.sendText(from, 'Maaf, url yang kamu kirim tidak valid')
+                        twitter(url)
+                            .then(async (videoMeta) => {
+                                try {
+                                    if (videoMeta.type == 'video') {
+                                        const content = videoMeta.variants.filter(x => x.content_type !== 'application/x-mpegURL').sort((a, b) => b.bitrate - a.bitrate)
+                                        const result = await urlShortener(content[0].url)
+                                        console.log('Shortlink: ' + result)
+                                        client.sendFileFromUrl(from, content[0].url, 'TwitterVideo.mp4', `Link Download: ${result} \n\nMaaf \nTerimakasih.`)
+                                    } else if (videoMeta.type == 'photo') {
+                                        for (var i = 0; i < videoMeta.variants.length; i++) {
+                                            await client.sendFileFromUrl(from, videoMeta.variants[i], videoMeta.variants[i].split('/media/')[1], '')
+                                        }
+                                    }
+                                } catch (err) {
+                                    client.sendText(from, `Error, ` + err)
+                                }
+                            }).catch((err) => {
+                                console.log(err)
+                                client.sendText(from, `Maaf, link tidak valid atau tidak ada video di link yang kamu kirim`)
+                            });
+                    }
+                    break
+                case '#fb':
+                case '#facebook':
+                        if (args.length == 2) {
+                            const url = args[1]
+                            if (!url.match(isUrl) && !url.includes('facebook.com')) return client.sendText(from, 'Maaf, url yang kamu kirim tidak valid')
+                            facebook(url)
+                                .then(async (videoMeta) => {
+                                    console.log(videoMeta)
+                                    try {
+                                        const shorthd = videoMeta.hd ? await urlShortener(videoMeta.hd) : 'Tidak Tersedia'
+                                        console.log('Shortlink: ' + shorthd)
+                                        const shortsd = videoMeta.sd ? await urlShortener(videoMeta.sd) : 'Tidak Tersedia'
+                                        console.log('Shortlink: ' + shortsd)
+                                        client.sendText(from, `Title: ${videoMeta.title} \nLink Download: \nHD Quality: ${shorthd} \nSD Quality: ${shortsd} \n\nMaaf \nTerimakasih.`)
+                                    } catch (err) {
+                                        client.sendText(from, `Error, ` + err)
+                                    }
+                                })
+                                .catch((err) => {
+                                    client.sendText(from, `Error, url tidak valid atau tidak memuat video \n${err}`)
+                                })
+                        }
+                        break
                 case 'assalamualaikum':
                     client.sendText(from, 'waalaikumsalam wr. wb.')
                     break
